4cd9a5bdde599ac5ba178e745770c1196e60c896
ResioIgnacio1996 <resioignacio@gmail.com>
2022-04-28 18:54:05 -0300

    add config data base

--- build/keys.js
+++ build/keys.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'resio',
-        password: 'resio96',
-        database: 'bdsos-soft'
+        host: 'us-cdbr-east-05.cleardb.net',
+        user: 'b5ea460de93e08',
+        password: '49d5ce3d',
+        database: 'heroku_d9a8d0a2e0460f4'
     }
 };
+
