95cb381feb70d76843b5acdffa270cfdaa5125fb
ResioIgnacio1996 <resioignacio@gmail.com>
2022-05-25 16:17:45 -0300

    .

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
-const serverHttp = __importDefault( require('http').server(app))
-const io = __importDefault(require('@type/socket.io')(serverHttp))
+//const serverHttp = __importDefault( require('http').server(app))
+//const io = __importDefault(require('@type/socket.io')(serverHttp))
 const indexRoutes_1 = __importDefault(require("./build/routes/indexRoutes"));
 const clienteRoutes_1 = __importDefault(require("./build/routes/clienteRoutes"));
 const diasRoutes_1 = __importDefault(require("./build/routes/diasRoutes"));
