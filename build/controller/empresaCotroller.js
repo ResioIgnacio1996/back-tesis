d6ceb21db69f85ba6c5f4cc55939a6b15b584514
ResioIgnacio1996 <resioignacio@gmail.com>
2022-06-14 20:58:48 -0300

    .

--- build/controller/empresaCotroller.js
+++ build/controller/empresaCotroller.js
@@ -343,14 +343,12 @@ setVistoEmpresa(req, res) {
 getUltimaNotificacion(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         console.log("entra ultima notificacion");
-        const idEmpresa = req.params.idEmpresa;
-        console.log(cliente)
-        
+        const idEmpresa = req.params.idEmpresa;        
         const empresas = yield database_1.default.query("SELECT max(n.id),n.descripcion, c.usuario, e.nombre, c.usuario, e.id, c.id as idCliente, n.idViaje from cliente as c  inner join notificacion as n on n.idCliente=c.id  inner join empresa as e on e.id= n.idEmpresa inner join viaje as v on v.id=n.idViaje where  n.idEmpresa=" + idEmpresa + " group by c.id,e.id");
         res.json(empresas);
     });
 }
 } 
 
-const clienteController = new EmpresaController();
-exports.default = clienteController;
+const empresaController = new EmpresaController();
+exports.default = empresaController;
