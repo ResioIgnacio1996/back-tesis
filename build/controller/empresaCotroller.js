5606a82b9b5c9816a8a4596d48ea41c213dd543c
ResioIgnacio1996 <resioignacio@gmail.com>
2022-06-30 19:09:07 -0300

    .

--- build/controller/empresaCotroller.js
+++ build/controller/empresaCotroller.js
@@ -164,7 +164,7 @@ class EmpresaController {
     getViajes(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             const idEmpresa = req.params.idEmpresa;
-            const empresas = yield database_1.default.query(" select v.idEmpresa,  c.id as idCliente, c.usuario, c.token, v.id, v.origen, v.latitud, v.longitud,v.destino,v.estado,v.distancia, v.id as idViaje,ev.descripcion from viaje as v inner join estado_viaje as ev on ev.codigo=v.estado inner join cliente as c on c.id= v.idCliente where (v.idEmpresa= " + [idEmpresa] + " and  v.fecha =date_add(NOW(), INTERVAL +1 DAY) or v.fecha=curdate()) order by v.fecha_modificacion desc");
+            const empresas = yield database_1.default.query(" select v.idEmpresa,  c.id as idCliente, c.usuario, c.token, v.id, v.origen, v.latitud, v.longitud,v.destino,v.estado,v.distancia, v.id as idViaje,ev.descripcion from viaje as v inner join estado_viaje as ev on ev.codigo=v.estado inner join cliente as c on c.id= v.idCliente where (v.idEmpresa= " + [idEmpresa] + " and  v.fecha <=date_add(NOW(), INTERVAL +1 DAY) ) order by v.fecha_modificacion desc");
             res.json(empresas);
         }); 
     }
