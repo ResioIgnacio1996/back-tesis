6cf398ce76933c8d1235042c00ab472b07d37445
ResioIgnacio1996 <resioignacio@gmail.com>
2022-08-27 01:07:58 -0300

    chat

--- Server/index.js
+++ Server/index.js
@@ -5,21 +5,43 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
-//const serverHttp = __importDefault( require('http').server(app))
-//const io = __importDefault(require('@type/socket.io')(serverHttp))
+const serverHttp = __importDefault(require('http').server(express_1.default))
 const indexRoutes_1 = __importDefault(require("./build/routes/indexRoutes"));
 const clienteRoutes_1 = __importDefault(require("./build/routes/clienteRoutes"));
 const diasRoutes_1 = __importDefault(require("./build/routes/diasRoutes"));
 const tipoVehiculoRoutes_1 = __importDefault(require("./build/routes/tipoVehiculoRoutes"));
 const comentarioRoutes_1 = __importDefault(require("./build/routes/comentarioRoutes"));
 const empresaRoutes_1 = __importDefault(require("./build/routes/empresaRoutes"))
-//const chatRoutes = __importDefault(require("./chats/index"))
+const chatRoutes = __importDefault(require("./chats/index"))
+const { Socket } = require('dgram');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: true,
+        credentials: true,
+        methods: ['GET', 'POST']
+    }
+});
+io.on('connection', (socket) => {
+    console.log("anda chat")
+    socket.on("sendMesagge", (object) => {
+        console.log("test= " + object.text)
 
+        socket.broadcast.emit("resiveMensagge", object);
+    })
+})
+app.get('/', (req, res) => {
+    res.send('<h1>hola mundo</>')
+});
+http.listen(3000, () => {
+    console.log("hola ando ")
+})
 class Server {
     constructor() {
         this.app = express_1.default();
         this.config();
-        this.routes(); 
+        this.routes();
     }
     config() {
         this.app.set('port', process.env.PORT);
@@ -35,13 +57,13 @@ class Server {
         this.app.use('/api/tipoVehiculo', tipoVehiculoRoutes_1.default);
         this.app.use('/api/comentario', comentarioRoutes_1.default);
         this.app.use('/api/empresa', empresaRoutes_1.default)
-      //  this.app.use('/api/chats',chatRoutes.default)
+        //  this.app.use('/api/chats',chatRoutes.default)
     }
     start() {
         this.app.listen(this.app.get('port'));
         console.log("escucho el puerto", this.app.get('port'));
     }
-    
+
 }
 /*io.on('connection',(socket)=>{
     const idHandShake=socket.id
