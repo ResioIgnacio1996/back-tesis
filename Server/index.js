f229cd93245d70c4bb86efa8189dfedb5e5ba77d
ResioIgnacio1996 <resioignacio@gmail.com>
2022-08-27 01:20:31 -0300

    .

--- Server/index.js
+++ Server/index.js
@@ -31,12 +31,7 @@ io.on('connection', (socket) => {
         socket.broadcast.emit("resiveMensagge", object);
     })
 })
-app.get('/', (req, res) => {
-    res.send('<h1>hola mundo</>')
-});
-http.listen(3000, () => {
-    console.log("hola ando ")
-})
+
 class Server {
     constructor() {
         this.app = express_1.default();
